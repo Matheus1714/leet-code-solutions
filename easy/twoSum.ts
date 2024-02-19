b8fcdcee7354c8c0c6acc4b6cc6696c90f52b240
matheus1714 <matheussilvamartins1714@gmail.com>
2024-02-18 23:52:07 -0300

    feat: change O(n^2) to O(n)

--- easy/twoSum.ts
+++ easy/twoSum.ts
@@ -1,13 +1,12 @@
 export function twoSum(nums: number[], target: number): number[] {
   const n = nums.length;
+  const mapNumbers = new Map();
   for (let i = 0; i < n; i++) {
-    const ai = nums[i];
-    for (let j = i + 1; j < n; j++) {
-      const aj = nums[j];
-      if (ai + aj === target) {
-        return [i, j];
-      }
+    const difference = target - nums[i];
+    if (mapNumbers.has(difference)) {
+      return [mapNumbers.get(difference), i];
     }
+    mapNumbers.set(nums[i], i);
   }
   throw new Error("No numbers were found in witch the sum is the target");
 }
